a41c21a280fe21e9cb4a0c5b311e79aff4bdb0c3
cameron skene <cameron.t.skene@gmail.com>
2018-11-26 23:07:57 +0100

    fixed routing -- this is huge

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,36 +1,39 @@
-import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Home from './Home';
-import AddCharity from './Charity/CRUD/AddCharity';
-import AddCampaign from './Campaign/CRUD/AddCampaign';
-import AddMaterial from './Material/CRUD/AddMaterial';
-import Login from './Authentication/Login';
-import Signup from './Authentication/Signup';
-import api from '../api';
-
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import AddCharity from "./Charity/CRUD/AddCharity";
+import AddCampaign from "./Campaign/CRUD/AddCampaign";
+import AddMaterial from "./Material/CRUD/AddMaterial";
+import Login from "./Authentication/Login";
+import Signup from "./Authentication/Signup";
+import api from "../api";
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     api.loadUser();
   }
 
   handleLogoutClick(e) {
-    api.logout()
+    api.logout();
   }
 
-
-  render() {  
-               
+  render() {
     return (
       <div className="App">
         <Switch>
-          <Route path="/" component={Home}/>
-          <Route path="/add-charity" component={AddCharity} />
-          <Route path={"/charities/:id/campaigns/add"} component={AddCampaign} />
-          <Route path={"/charities/:charid/campaigns/:campid/materials/add"} component={AddMaterial} />
+          <Route path="/charities/add" component={AddCharity} />
+          <Route
+            path={"/charities/:id/campaigns/add"}
+            component={AddCampaign}
+          />
+          <Route
+            path={"/charities/:charid/campaigns/:campid/materials/add"}
+            component={AddMaterial}
+          />
           <Route path="/signup" component={Signup} />
           <Route path="/login" component={Login} />
+          <Route path="/" component={Home} />
           <Route render={() => <h2>404- Page not found</h2>} />
         </Switch>
       </div>
