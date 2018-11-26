0c34b10a231791875a9f326544a15fc9e2fc87e5
cameron skene <cameron.t.skene@gmail.com>
2018-11-26 20:16:30 +0100

    changed isActive CharityCard handling for smoother user experience

--- client/src/components/Charity/CharityCard.js
+++ client/src/components/Charity/CharityCard.js
@@ -1,28 +1,41 @@
-import React, { Component } from 'react';
-import { ListGroupItem, Row, Col } from 'reactstrap';
-import { Link } from 'react-router-dom';
-
+import React, { Component } from "react";
+import { ListGroupItem, Row, Col } from "reactstrap";
+import { Link } from "react-router-dom";
 
 class CharityCard extends Component {
   handleCharityClick() {
-    this.props.handleActiveCharity(this.props.data._id)
-  };
+    this.props.handleActiveCharity(this.props.data._id);
+  }
 
   render() {
-    const { data, activeCharity } = this.props;  
+    const { data, activeCharity } = this.props;
     return (
       <div className="CharityCard">
-        <ListGroupItem active={data._id === activeCharity._id} className="thin-card" onClick={this.handleCharityClick.bind(this)}>
+        <ListGroupItem
+          active={data._id === activeCharity || data._id === activeCharity._id}
+          className="thin-card"
+          onClick={this.handleCharityClick.bind(this)}
+        >
           <Link to={"/charities/" + data._id}>
-          <Row> 
-          <Col md="6" className="no-border flex"><div className="whitespace flex"><img className="thin-card-img" src={data.pictureUrl} alt={"Image of " + data.name} /></div></Col>
-          <Col md="6" className="no-border flex"><div className="thin-card-txt">{data.name}</div></Col>
-          </Row>
+            <Row>
+              <Col md="6" className="no-border flex">
+                <div className="whitespace flex">
+                  <img
+                    className="thin-card-img"
+                    src={data.pictureUrl}
+                    alt={"Image of " + data.name}
+                  />
+                </div>
+              </Col>
+              <Col md="6" className="no-border flex">
+                <div className="thin-card-txt">{data.name}</div>
+              </Col>
+            </Row>
           </Link>
         </ListGroupItem>
       </div>
     );
   }
-};          
+}
 
 export default CharityCard;
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,64 +1,66 @@
-import React, { Component } from 'react';
-import { Row, Col, Container } from 'reactstrap';
-import { Route } from 'react-router-dom';
-import Search from './Search';
-import CharityList from './Charity/CharityList';
-import CampaignList from './Campaign/CampaignList';
-import MaterialList from './Material/MaterialList';
-import api from '../api';
+import React, { Component } from "react";
+import { Row, Col, Container } from "reactstrap";
+import { Route } from "react-router-dom";
+import Search from "./Search";
+import CharityList from "./Charity/CharityList";
+import CampaignList from "./Campaign/CampaignList";
+import MaterialList from "./Material/MaterialList";
+import api from "../api";
 
 class Home extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       data: [{ name: "" }],
       activeCharity: { name: "" },
       activeCampaign: {}
-    }
+    };
   }
 
   componentDidMount() {
-    api.getCharities()
-    .then(result => {
-      this.setState({
-        data: result
+    api
+      .getCharities()
+      .then(result => {
+        this.setState({
+          data: result
+        });
+        // persist through refreshes + allow for link sharing
+        const pathname = this.props.location.pathname;
+        if (pathname !== "/") {
+          let charid = pathname.substring(11, 35);
+          api.getCharity(charid).then(result => {
+            this.setState({
+              activeCharity: result
+            });
+          });
+        }
       })
-      const pathname = this.props.location.pathname;
-      if(pathname !== '/') {
-        let charid = pathname.substring(11, 35);
-        api.getCharity(charid)
-        .then(result => {
-          this.setState({
-            activeCharity: result
-          })
-        })
-      }
-    })
-    .catch(err => console.log(err))
+      .catch(err => console.log(err));
   }
-  
 
   handleSearchQuery(searchQuery) {
-    api.getCharitiesByQuery(searchQuery)
-    .then(result => {
+    api.getCharitiesByQuery(searchQuery).then(result => {
       this.setState({
         data: result
-      })
-    })
-  };
+      });
+    });
+  }
 
-  handleActiveCharity(activeCharityID){
-    api.getCharity(activeCharityID)
-    .then(result => {
+  handleActiveCharity(activeCharityID) {
+    // this handles the isActive for the clicked card while the async call is being made -- makes the user's click feel more 'instant'
+    this.setState({
+      activeCharity: activeCharityID
+    });
+    api.getCharity(activeCharityID).then(result => {
       this.setState({
         activeCharity: result
-      })
-    })
+      });
+    });
   }
-  
+
   render() {
     // const CampaignsRoute = ({ data }) => (
-    //   <Route path="/charities/:charid/campaigns" 
+    //   <Route path="/charities/:charid/campaigns"
     //   render={ (props) =>  <CampaignList {...props} data={data}/> }
     //   />
     // );
@@ -70,17 +72,21 @@ class Home extends Component {
     return (
       <div className="Home">
         <Search handleSearchQuery={this.handleSearchQuery.bind(this)} />
-        <Container> 
+        <Container>
           <Row>
-            <Col className="home-col"> 
+            <Col className="home-col">
               <h4>Charities</h4>
-              <CharityList  data={this.state.data} activeCharity={this.state.activeCharity} handleActiveCharity={this.handleActiveCharity.bind(this)} />
+              <CharityList
+                data={this.state.data}
+                activeCharity={this.state.activeCharity}
+                handleActiveCharity={this.handleActiveCharity.bind(this)}
+              />
             </Col>
-            <Col className="home-col"> 
+            <Col className="home-col">
               {/* <h4>Campaigns</h4> 
               <CampaignsRoute data={} />  */}
             </Col>
-            <Col className="home-col" xs="6"> 
+            <Col className="home-col" xs="6">
               {/* <h4>Materials</h4>
               <MaterialsRoute data={} />  */}
             </Col>
