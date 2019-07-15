3fae5aba4a3a7183f7a522c429abf1eb1ac775ed
Cameron Skene <cameron@i19s.com>
2019-07-15 10:40:40 +0200

    npm i

--- client/src/components/Material/MaterialModal.js
+++ client/src/components/Material/MaterialModal.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import React from "react";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 
 class MaterialModal extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       modal: this.props.modal,
-      data: this.props.data,
+      data: this.props.data
     };
 
     this.toggle = this.toggle.bind(this);
   }
   componentWillReceiveProps(newProps) {
-    this.setState({modal: newProps.modal})
+    this.setState({ modal: newProps.modal });
   }
 
   toggle() {
@@ -28,10 +28,16 @@ class MaterialModal extends React.Component {
         <Modal size="lg" isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
           <ModalHeader toggle={this.toggle}>{data.title}</ModalHeader>
           <ModalBody>
-            <img src={data.pictureUrl} className="modal-image" alt={"Image of " + data.title + " from " + data._charity}/>
+            <img
+              src={data.pictureUrl}
+              className="modal-image"
+              alt={"Image of " + data.title + " from " + data._charity}
+            />
           </ModalBody>
           <ModalFooter>
-            <Button color="secondary" onClick={this.toggle}>Back</Button>
+            <Button color="secondary" onClick={this.toggle}>
+              Back
+            </Button>
           </ModalFooter>
         </Modal>
       </div>
